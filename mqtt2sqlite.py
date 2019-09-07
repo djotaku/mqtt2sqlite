9dbf52a42af5bb5eda1665020d675eeae8ad7ff1
Eric Mesa <ericsbinaryworld@gmail.com>
2019-09-07 14:42:15 -0400

    right now it's working - kind of. Need to delete database each time before starting. Also, not inputing data yet

--- mqtt2sqlite.py
+++ mqtt2sqlite.py
@@ -1,6 +1,14 @@
 import paho.mqtt.subscribe as subscribe
+import sqlite3
+
+conn = sqlite3.connect('smoker.db')
+c = conn.cursor()
 
 topics = ['outTopic','smoker/temp','smoker/WiFi/#']
+
+for topic in topics:
+    c.execute('''CREATE TABLE '%s' 
+              (date text, value text)''' % topic)
 
 while True:
     msg = subscribe.simple(topics, hostname="tanukimario.mushroomkingdom", retained=False, msg_count=2) #in order not to miss any messages you will want msg_count to equal the max number of messages you expect to be sent at once.
@@ -10,5 +18,10 @@ while True:
             tempF = 9.0/5.0 * int(message.payload)+32
             print(f'{message.topic}:{tempC}° C')
             print(f'{message.topic}:{tempF}° F')
+            #c.execute("INSERT INTO '%s' VALUES ('fakedate, '%s')" % (message.topic,str(tempF)))
+            c.execute("INSERT INTO 'smoker/temp' VALUES ('fakedate','fakemessage')")
+            conn.commit()
         else:
             print(f'{message.topic}:{message.payload}')
+            
+conn.close()
